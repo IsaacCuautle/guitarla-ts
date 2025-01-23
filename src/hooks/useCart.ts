a9b78df7991f02d3b02127196ee5fdd2bba7b7f5
Luis Isaac Cuautle Delgadillo <cuautle.tron@gmail.com>
2025-01-23 17:32:33 -0600

    Incrementar y decrementar la cantidad de un producto 2

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -22,34 +22,6 @@ const useCart = () => {
     }, [cart])
     
 
-    
-
-    // //* Decrementa la cantidad de un producto
-    // function decreaseQuantity(id : Guitar['id']) {
-
-    //     // Crea una copia del carrito he itera sobre ella
-    //     const updatedCart = cart.map( item => {
-
-    //     // Si el item coincide con el id, decrementa la cantidad de ese item en 1
-    //     if(item.id === id && item.quantity > 1)
-    //     {
-
-    //         return{
-    //         ...item,
-    //         quantity: item.quantity - 1
-    //         }
-
-    //     }
-
-    //     // Mantiene los demas elementos
-    //     return item
-    //     })
-
-    //     // Setea el carrito
-    //     setCart(updatedCart);
-    
-    // }
-
     //* Limpia el carrito
     function cleanCart() {
         
